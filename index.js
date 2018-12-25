e7421bb34457698e357218deb54f3b96c6fc4627
kovner chalumo <kchalumo@gmail.com>
2018-12-25 02:58:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 function shout(string) { return string.toUpperCase(); }
 function whisper(string) { return string.toLowerCase(); }
+function logShout(string) { console.log(shout(string)); }
+function logWhisper(string) { console.log(whisper(string)); }
